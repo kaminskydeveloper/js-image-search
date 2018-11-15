45b06566c136aed5e6ddcedfd55a1e9d1eb8d3d6
Damian <damiankaminski22@gmail.com>
2018-11-15 18:50:39 +0100

    API has been fixed

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const API_URL =
-  "https://api.unsplash.com/photos/?client_id=5cb163c5a827aa01ffc34e7ffc47ab094674c23fdfc309b70124746c7fe3194d";
+  "https://api.unsplash.com/search/photos/?client_id=5cb163c5a827aa01ffc34e7ffc47ab094674c23fdfc309b70124746c7fe3194d";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const loadingImage = document.querySelector("#loadingImage");
 const imageSection = document.querySelector(".images");
 
-form.addEventListener("submit", formSubmitted);
-
 loadingImage.style.display = "none";
+
+form.addEventListener("submit", formSubmitted);
 
 function formSubmitted(event) {
   event.preventDefault();
@@ -20,18 +20,20 @@ function formSubmitted(event) {
 function search(searchTerm) {
   const url = `${API_URL}&query=${searchTerm}`;
   loadingImage.style.display = "";
+  imageSection.innerHTML = "";
   console.log(url);
   return fetch(url)
     .then(response => response.json())
     .then(result => {
-      return result;
+      console.log(result);
+      return result.results;
     });
 }
 
 function displayImages(images) {
   images.forEach(image => {
     const imageElement = document.createElement("img");
-    imageElement.src = image.urls.raw;
+    imageElement.src = image.urls.full;
     imageSection.appendChild(imageElement);
   });
   loadingImage.style.display = "none";
